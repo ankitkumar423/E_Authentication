23242a98b1fc40c1181995e3316ceaa377635098
ankitkkr <aaa2000kumar@gmail.com>
2022-06-27 13:36:16 +0530

    user_interface

--- app/urls.py
+++ app/urls.py
@@ -1,5 +1,13 @@
 from django.contrib import admin
 from django.urls import path,include
+from app.views import *
 
 urlpatterns = [
+    path('', signup,name='signup'),
+    path('signup', signup,name='signup'),
+    path('login', login,name='login'),
+    path('home', home,name='home'),
+    path('changepass', changepass, name='changepass'),
+    path('logout', logout,name='logout'),
 ]
+
